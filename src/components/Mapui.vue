34693a949c3149130739624e428dee10e270b0ae
ralgr <kimtorres40@yahoo.com>
2019-04-19 12:56:40 +0100

    added more items in floating map ui

--- src/components/Mapui.vue
+++ src/components/Mapui.vue
@@ -1,38 +1,76 @@
 <template lang="html">
   <div class="mapui">
 
-    <v-layout>
-      <v-flex xs12>
-        <v-card>
-          <v-img
-            src="https://cdn.vuetifyjs.com/images/cards/desert.jpg"
-            aspect-ratio="2.75"
-          ></v-img>
+    <v-layout row wrap>
+      <v-flex xs8>
 
-          <v-card-actions>
-            <v-flex xs12 d-flex>
-              <v-select
-                :items="groupCont"
-                label="Group Context"
-              ></v-select>
+        <v-card>
+
+          <v-layout row wrap justify-center>
+
+            <v-flex xs8>
+
+              <v-responsive>
+                <v-card-actions>
+                  <v-flex xs3 d-flex>
+                    <v-select
+                      :items="groupCont"
+                      label="Group Context"
+                    ></v-select>
+                  </v-flex>
+                </v-card-actions>
+                <v-card-actions>
+                  <v-flex xs3 d-flex>
+                    <v-select
+                      :items="stopTypes"
+                      label="Location Type"
+                    ></v-select>
+                  </v-flex>
+                </v-card-actions>
+
+                <v-list two-line>
+                  <template v-for="(item, index) in items">
+                    <v-subheader
+                      v-if="item.header"
+                      :key="item.header"
+                    >
+                      {{ item.header }}
+                    </v-subheader>
+
+                    <v-divider
+                      v-else-if="item.divider"
+                      :key="index"
+                      :inset="item.inset"
+                    ></v-divider>
+
+                    <v-list-tile
+                      v-else
+                      :key="item.title"
+                      avatar
+                      @click=""
+                    >
+                      <v-list-tile-avatar>
+                        <img :src="item.avatar">
+                      </v-list-tile-avatar>
+
+                      <v-list-tile-content>
+                        <v-list-tile-title v-html="item.title"></v-list-tile-title>
+                        <v-list-tile-sub-title v-html="item.subtitle"></v-list-tile-sub-title>
+                      </v-list-tile-content>
+                    </v-list-tile>
+                  </template>
+                </v-list>
+
+              </v-responsive>
+
             </v-flex>
-          </v-card-actions>
-          <v-card-actions>
-            <v-flex xs12 d-flex>
-              <v-select
-                :items="stopTypes"
-                label="Location Typ"
-              ></v-select>
-            </v-flex>
-          </v-card-actions>
+
+          </v-layout>
+
         </v-card>
+
       </v-flex>
     </v-layout>
-
-      <v-layout row wrap>
-
-
-      </v-layout>
 
   </div>
 </template>
@@ -59,7 +97,27 @@ export default {
         'Points of interest',
         'Attraction',
         'Tour',
-      ]
+      ],
+      items: [
+          { header: 'Today' },
+          {
+            avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
+            title: 'Brunch this weekend?',
+            subtitle: "<span class='text--primary'>Ali Connors</span> &mdash; I'll be in your neighborhood doing errands this weekend. Do you want to hang out?"
+          },
+          { divider: true, inset: true },
+          {
+            avatar: 'https://cdn.vuetifyjs.com/images/lists/2.jpg',
+            title: 'Summer BBQ <span class="grey--text text--lighten-1">4</span>',
+            subtitle: "<span class='text--primary'>to Alex, Scott, Jennifer</span> &mdash; Wish I could come, but I'm out of town this weekend."
+          },
+          { divider: true, inset: true },
+          {
+            avatar: 'https://cdn.vuetifyjs.com/images/lists/3.jpg',
+            title: 'Oui oui',
+            subtitle: "<span class='text--primary'>Sandra Adams</span> &mdash; Do you have Paris recommendations? Have you ever been?"
+          }
+        ]
     }
   }
 }
